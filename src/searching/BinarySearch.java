8d7691ce66c4c4ca1bcd00cf89e6017cc60abafd
Kofi Gyawu <kofi.gyawu@amalitech.org>
2024-10-25 13:59:11 +0000

    feat: add sort and search

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -2,4 +2,27 @@ package searching;
 
 public class BinarySearch {
 
+
+    public int search(int[] array, int target) {
+        return search(array, target, 0, array.length - 1);
+    }
+
+
+    private int search(int[] array, int target, int left, int right) {
+        if (right < left) {
+            return -1;
+        }
+
+        int middle = (left + right) / 2;
+
+        if (array[middle] == target) {
+            return middle;
+        }
+
+        if (target < array[middle]) {
+            return search(array, target, left, middle - 1);
+        }
+
+        return search(array, target, middle + 1, right);
+    }
 }
---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,21 @@
+package sorting;
+
+public class SelectionSort {
+
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the minimum element found with the first element of the unsorted part
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+
+}
